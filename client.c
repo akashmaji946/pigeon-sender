bdc3fdfd67c2e28cea0be8199113d272f709452c
surajmaji10 <akashmaji728@gmail.com>
2025-01-25 18:19:20 +0530

    first commit

---
+++ client.c
@@ -0,0 +1,77 @@
+#include "pigeon.h"
+
+
+bool sendEmail(Email* email, Server* server){
+
+    int8 buf[256];
+    zero(buf, 256);
+
+    // establish TCP connection
+    struct sockaddr_in sock;
+
+    signed int tmp;
+    int16 s;
+
+    tmp = socket(AF_INET, SOCK_STREAM, 0);
+    assert(tmp > 0);
+
+    s = (int16)tmp;
+
+    sock.sin_family = AF_INET;
+    sock.sin_port = htons(587);
+    sock.sin_addr.s_addr = server->ip;
+
+    tmp = connect($i s, (struct sockaddr*)&sock, sizeof(struct sockaddr));
+    assert(tmp == 0);
+
+    // check
+    printf("Connected to %s...\n", server->server);
+
+    zero(buf, 256);
+    tmp = read($i s, $c buf, 255);
+
+    dprintf($i s, "EHLO %s\n", email->domain);
+
+    zero(buf, 256);
+    tmp = read($i s, $c buf, 255);
+    printf("From Server: %s\n", $c buf);
+
+    // close
+    close(s);
+
+    return true;
+
+
+}
+
+void sendMailHelper(char* from_addr, char* server_addr, char* ip){
+    Email* email;
+    Server* server;
+
+    email = malloc(sizeof(Email));
+    server = malloc(sizeof(Server));
+
+    memset(email, 0, sizeof(Email));
+    memset(server, 0, sizeof(Server));
+
+    strncpy(email->src, from_addr, 63);
+    email->domain = email->src + 13;
+    strncpy(server->server, server_addr, 63);
+    server->ip = inet_addr(ip);
+    printf("______________Calling_______________\n");
+    bool ret = sendEmail(email, server);
+    free(email);
+    free(server);
+
+    printf("______________Success_______________\n");
+
+}
+
+// will be removed later
+int main(int argc, char* argv[]){
+
+    sendMailHelper("akashmaji945@gmail.com", 
+    "smtp.gmail.com", "142.251.12.109");
+
+    return 0;
+}
---
+++ pigeon.c
@@ -0,0 +1,6 @@
+/* pigeon.c */
+#include "pigeon.h"
+
+int main(int argc, char *argv[]) {
+    return 0;
+}
